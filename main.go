e175b688f3effbd5232b9e079f9e8996f83c2052
Zak <89150707+Zak0749@users.noreply.github.com>
2021-12-17 18:35:47 +0000

    ed

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"peg-solitare/moves"
+	"peg-solitare/starting"
+	"peg-solitare/types"
+	"peg-solitate/pretify"
+)
 
 func main() {
-	fmt.Println("hello world")
+	jobs := make(chan types.Board, 1)
+	results := make(chan []types.Board, 1)
+
+	go worker(jobs, results)
+
+	for i := 0; i < 1; i++ {
+		jobs <- starting.Board
+	}
+
+	for j := 0; j < 1; j++ {
+		res := <-results
+		for _, board := range res {
+			fmt.Println(pretify.Board(board))
+		}
+	}
+}
+
+func worker(jobs <-chan types.Board, results chan<- []types.Board) {
+	for n := range jobs {
+		results <- moves.GetMoves(n)
+	}
 }
